6e6dbedad275b0d73ef1610e0d3ce5ab0e3dc480
Rasmus Porsager <rasmus@porsager.com>
2020-01-16 13:34:38 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const target =
+     typeof self !== 'undefined' && self
+  || typeof window !== 'undefined' && window
+  || typeof global !== 'undefined' && global
+
+target && (target.p = target.plog = p)
+
+function p(x) {
+  if (Array.isArray(x) && Array.isArray(x.raw)) {
+    const prefix = String.raw.apply(String, arguments)
+    return function(first) {
+      console.log.apply(console, [prefix].concat(Array.from(arguments)))
+      return first
+    }
+  }
+
+  console.log.apply(console, arguments)
+  return x
+}
